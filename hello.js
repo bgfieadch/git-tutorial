3ac559863bc50a25859d530176eec16899842f24
bgfieadch <bgfieadch@qq.com>
2020-02-07 21:32:47 +0800

    add nodejs test

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-}).listen(8081);
+const express = require('express')
+const app = express()
+const port = 3000
 
-console.log('Server running at http://127.0.0.1:8081/');
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
